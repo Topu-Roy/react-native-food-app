d172b6114275ccec9217e9793ef6961307601622
topu-roy <topuroy143333@gmail.com>
2024-02-14 15:18:59 +0600

    TypeScript - added default image if uri is null

--- src/components/product/ProductListItem.tsx
+++ src/components/product/ProductListItem.tsx
@@ -4,15 +4,17 @@ type ProductListItemType = {
     product: {
         id: number;
         name: string;
-        image: string;
+        image: string | null;
         price: number;
     }
 }
 
+export const defaultImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+
 export default function ProductListItem({ product }: ProductListItemType) {
     return (
         <View className='bg-stone-200 p-2 rounded-md'>
-            <Image source={{ uri: product.image }} style={styles.image} />
+            <Image source={{ uri: product.image || defaultImage }} style={styles.image} />
             <Text className='text-base font-bold' >{product.name}</Text>
             <Text className='font-semibold text-blue-700 dark:text-blue-400'>${product.price}</Text>
         </View>
