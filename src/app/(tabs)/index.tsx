88546654541820f24e4bdcc64e9520509bf6a2e6
topu-roy <topuroy143333@gmail.com>
2024-02-14 21:01:26 +0600

    Style - Gap between the cards

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -9,6 +9,8 @@ export default function TabOneScreen() {
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
     />
   );
 }
