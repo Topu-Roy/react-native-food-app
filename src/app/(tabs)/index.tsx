ac693c4bff266a633eca238310ce4cb614538403
topu-roy <topuroy143333@gmail.com>
2024-02-14 15:05:51 +0600

    UI - ProductCard component on own file

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,15 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView } from 'react-native';
 import products from '@/assets/data/products';
-
-const firstProduct = products[0]
+import ProductCard from '@/src/components/product/ProductCard';
 
 export default function TabOneScreen() {
   return (
-    <View className='bg-stone-200 p-2 rounded-md'>
-      <Image source={{uri: firstProduct.image}} style={styles.image}/>
-      <Text className='text-base font-bold' >{firstProduct.name}</Text>
-      <Text className='font-semibold text-blue-700 dark:text-blue-400'>${firstProduct.price}</Text>
-    </View>
+    <ScrollView>
+      <ProductCard product={products[0]} />
+      <ProductCard product={products[1]} />
+      <ProductCard product={products[2]} />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    aspectRatio: 1
-  }
-});
+
---
+++ src/components/product/ProductCard.tsx
@@ -0,0 +1,28 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+type ProductType = {
+    product: {
+        id: number;
+        name: string;
+        image: string;
+        price: number;
+    }
+}
+
+export default function ProductCard(props: ProductType) {
+    const { product } = props;
+    return (
+        <View className='bg-stone-200 p-2 rounded-md'>
+            <Image source={{ uri: product.image }} style={styles.image} />
+            <Text className='text-base font-bold' >{product.name}</Text>
+            <Text className='font-semibold text-blue-700 dark:text-blue-400'>${product.price}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        aspectRatio: 1
+    }
+});
