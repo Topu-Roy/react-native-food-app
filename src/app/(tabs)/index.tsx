4db44c8bdb8e20e483b66ce35d3be679e55731fe
topu-roy <topuroy143333@gmail.com>
2024-02-14 21:06:58 +0600

    removed unnecessary style

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import ProductListItem from '@/src/components/product/ProductListItem';
 export default function TabOneScreen() {
   return (
     <FlatList
-      className='m-1'
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
