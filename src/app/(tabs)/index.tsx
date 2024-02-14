4e9bb9cc2d90d00db7a9d388d56bfc0d9676d940
topu-roy <topuroy143333@gmail.com>
2024-02-14 15:28:59 +0600

    UI - Added flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
-import { ScrollView } from 'react-native';
+import { FlatList, View } from 'react-native';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/product/ProductListItem';
 
 export default function TabOneScreen() {
   return (
-    <ScrollView>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-      <ProductListItem product={products[2]} />
-    </ScrollView>
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
+    </View>
   );
 }
 
