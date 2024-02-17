da672ae7010862eb863ae7688fad676924e5d751
topu-roy <topuroy143333@gmail.com>
2024-02-17 15:04:33 +0600

    added the cart modal to all menu layout screens

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Stack } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
 
-import Colors from '../../../constants/Colors';
-import { useColorScheme } from '../../../components/useColorScheme';
+import Colors from "../../../constants/Colors";
+import { useColorScheme } from "../../../components/useColorScheme";
 
 export default function layout() {
     const colorScheme = useColorScheme();
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{
-                title: "Menu",
+        <Stack
+            screenOptions={{
                 headerRight: () => (
                     <Link href="/cart" asChild>
                         <Pressable>
@@ -19,14 +18,21 @@ export default function layout() {
                                 <FontAwesome
                                     name="shopping-cart"
                                     size={25}
-                                    color={Colors[colorScheme ?? 'light'].text}
+                                    color={Colors[colorScheme ?? "light"].text}
                                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                                 />
                             )}
                         </Pressable>
                     </Link>
                 ),
-            }} />
+            }}
+        >
+            <Stack.Screen
+                name="index"
+                options={{
+                    title: "Menu",
+                }}
+            />
         </Stack>
-    )
+    );
 }
