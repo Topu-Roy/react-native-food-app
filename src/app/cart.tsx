1450cb9ac748beab5c9d16dfbc9c7556e2791743
topu-roy <topuroy143333@gmail.com>
2024-02-19 17:01:29 +0600

    UI - added the checkout button

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, FlatList, Platform } from "react-native";
+import { View, FlatList, Platform, Pressable, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "../provider/CartProvider";
 import { Stack } from "expo-router";
@@ -7,14 +7,23 @@ import CartListItem from "../components/cart/CartListItem";
 
 
 const CartModal = () => {
-    const { items } = useCart();
+    const { items, totalPrice } = useCart();
     return (
-        <View className="flex-1 bg-slate-200/60">
+        <View className="flex-1 bg-slate-200/60 p-3">
             <FlatList
                 data={items}
                 renderItem={({ item }) => <CartListItem item={item} />}
-                contentContainerStyle={{ gap: 10, padding: 10 }}
+                contentContainerStyle={{ gap: 10 }}
             />
+
+            <View className="gap-2">
+                <Text className="font-semibold text-xl">Total price: ${totalPrice}</Text>
+                <Pressable className='justify-center items-center h-14 bg-teal-400 rounded-full'>
+                    <Text className='text-lg font-medium'>
+                        Add to cart
+                    </Text>
+                </Pressable>
+            </View>
 
 
             {/* Conf stuff */}
